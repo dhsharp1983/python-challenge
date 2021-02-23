b70f5dab27efca4e32e8d792f7dbfbc91ae4211b
dhsharp1983 <sharpdavid83@gmail.com>
2021-02-23 14:22:35 +1100

    Completed PyPoll

--- PyPoll/PyPoll.py
+++ PyPoll/PyPoll.py
@@ -35,51 +35,16 @@ for rows in ImportedCSVData:
 # Calculate candidate results and place into dictionary
 CandidateResults = {}
 CandidateResults = dict((candidates,VotesList.count(candidates)) for candidates in set(VotesList))
-print(CandidateResults)
 
-# Duplicate Results dictionary and use for percentage calculations
-# CandidatePercentages = {}
-# CandidatePercentages = CandidateResults.copy()
-# print(CandidatePercentages)
-# for k,v in CandidatePercentages.items():
-#     print(v)
-#     CandidatePercentages[v] = 'bob'
-#     print(CandidatePercentages[v])
-
+# Duplicate CandidateResults dictionary, replace values with percentage figures 
 CandidatePercentages = dict((k, v / VoteCount) for k, v in CandidateResults.items())
-print(CandidatePercentages)
 
-
-
-# # Duplicate CandidateResults dictionary, and use for percentage calculations
-# CandidatePercentages = CandidateResults
-# for k,v in CandidatePercentages.items():
-#     v = v / VoteCount
-
-# print(CandidatePercentages["Khan"])
-
-
-# # Extract Candidate names from first dictionary, create second with percentages 
-# CandidatePercentages = {}
-# for key,value in CandidateResults.items():
-#     print(key)
-#     print(value)
-#     CandidatePercentages = dict([
-#         (value, "bob")
-# #        (key, "{:.1%}".format(value / VoteCount))
-#     ])
-
-#print(CandidateResults)
-# for key,value in CandidatePercentages.items():
-#     print(key)
-#     print(value)
-
+# Close CSV file
 csvfile.close()
+
+# Sort dictionaries into decending order
 SortedCandidates = dict(sorted(CandidateResults.items(), key=lambda x: x[1], reverse=True))
 SortedCandidatePercentages = dict(sorted(CandidatePercentages.items(), key=lambda x: x[1], reverse=True))
-print(SortedCandidates)
-print(SortedCandidatePercentages)
-
 
 # # print analysis
 print(f"  ")
@@ -88,26 +53,32 @@ print(f"-------------------------")
 print(f"Total Votes: {VoteCount}")
 print(f"-------------------------")
 for candidates in SortedCandidates:
-    # print(f"{candidates}: {SortedCandidates[candidates]} ({SortedCandidatePercentages[candidates]})")
     print(str(candidates) + ": " "{:.1%}".format(float(SortedCandidatePercentages[candidates])) + " (" + str(SortedCandidates[candidates]) + ")")
 print(f"-------------------------")
 for winner in SortedCandidates:
     print(f"Winner is: {winner}")
     break
 print(f"-------------------------")
-# print(f"Khan: 63.000% (2218231)")
-# print(f"Correy: 20.000% (704200)")
-# print(f"Li: 14.000% (492940)")
-# print(f"O'Tooley: 3.000% (105630)")
-# print(f"-------------------------")
-# print(f"Winner: Khan")
-# print(f"-------------------------")
 
-
-    
-
-# for key, value in CandidateResults.items():
-#     print(f"{key} received {value} votes")
+# Print Analysis to File
+# Delete previous analysis 
+filepath = os.path.join("..","Analysis","PyPoll_Analysis.txt")
+if os.path.exists(filepath):
+    os.remove(filepath)
+# Create File and write Analysis
+with open(filepath,"w") as writefile:
+    print(f"  ", file=writefile)
+    print(f"Election Results", file=writefile)
+    print(f"-------------------------", file=writefile)
+    print(f"Total Votes: {VoteCount}", file=writefile)
+    print(f"-------------------------", file=writefile)
+    for candidates in SortedCandidates:
+        print(str(candidates) + ": " "{:.1%}".format(float(SortedCandidatePercentages[candidates])) + " (" + str(SortedCandidates[candidates]) + ")", file=writefile)
+    print(f"-------------------------", file=writefile)
+    for winner in SortedCandidates:
+        print(f"Winner is: {winner}", file=writefile)
+        break
+    print(f"-------------------------", file=writefile)
 
 
 
