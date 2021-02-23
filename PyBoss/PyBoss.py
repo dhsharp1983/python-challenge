b4f996e5899fe81bb9c7ee6d9772e2fdf58e0f24
dhsharp1983 <sharpdavid83@gmail.com>
2021-02-23 14:56:44 +1100

    Before Editing PyBoss Data

---
+++ PyBoss/PyBoss.py
@@ -0,0 +1,73 @@
+# Convert From:
+# Emp ID,Name,DOB,SSN,State
+# 214,Sarah Simpson,1985-12-04,282-01-8166,Florida
+# 15,Samantha Lara,1993-09-08,848-80-7526,Colorado
+
+# Convert To:
+# Emp ID,First Name,Last Name,DOB,SSN,State
+# 214,Sarah,Simpson,MM/DD/YYYY,***-**-8166,FL
+# 15,Samantha,Lara,09/08/1993,***-**-7526,CO
+
+
+import os
+import csv
+
+def AbbreviateState(LongStateString):
+    us_state_abbrev = {
+    'Alabama': 'AL',
+    'Alaska': 'AK',
+    'Arizona': 'AZ',
+    'Arkansas': 'AR',
+    'California': 'CA',
+    'Colorado': 'CO',
+    'Connecticut': 'CT',
+    'Delaware': 'DE',
+    'Florida': 'FL',
+    'Georgia': 'GA',
+    'Hawaii': 'HI',
+    'Idaho': 'ID',
+    'Illinois': 'IL',
+    'Indiana': 'IN',
+    'Iowa': 'IA',
+    'Kansas': 'KS',
+    'Kentucky': 'KY',
+    'Louisiana': 'LA',
+    'Maine': 'ME',
+    'Maryland': 'MD',
+    'Massachusetts': 'MA',
+    'Michigan': 'MI',
+    'Minnesota': 'MN',
+    'Mississippi': 'MS',
+    'Missouri': 'MO',
+    'Montana': 'MT',
+    'Nebraska': 'NE',
+    'Nevada': 'NV',
+    'New Hampshire': 'NH',
+    'New Jersey': 'NJ',
+    'New Mexico': 'NM',
+    'New York': 'NY',
+    'North Carolina': 'NC',
+    'North Dakota': 'ND',
+    'Ohio': 'OH',
+    'Oklahoma': 'OK',
+    'Oregon': 'OR',
+    'Pennsylvania': 'PA',
+    'Rhode Island': 'RI',
+    'South Carolina': 'SC',
+    'South Dakota': 'SD',
+    'Tennessee': 'TN',
+    'Texas': 'TX',
+    'Utah': 'UT',
+    'Vermont': 'VT',
+    'Virginia': 'VA',
+    'Washington': 'WA',
+    'West Virginia': 'WV',
+    'Wisconsin': 'WI',
+    'Wyoming': 'WY',
+    }
+    ShortStateString = us_state_abbrev.get(LongStateString)
+    return ShortStateString
+
+
+LongStateString = input("What state?")
+print(AbbreviateState(LongStateString))
